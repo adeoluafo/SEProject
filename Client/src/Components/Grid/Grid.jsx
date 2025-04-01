628917217ff7951f06d26f477d1e720f8a711a9d
Chinyere offor <obiagelichinyere566@gmail.com>
2025-04-01 15:57:31 -0400

    search functionality completed

--- Client/src/Components/Grid/Grid.jsx
+++ Client/src/Components/Grid/Grid.jsx
@@ -6,28 +6,93 @@ import { RecipesContext } from "../../UserContext";
 
 export default function Grid({ duration, difficulty, cuisine, diet, keyword }) {
   const { recipesContext } = useContext(RecipesContext);
-  // function whatToParse() {
-  //   let tempRecipes = [...recipesContext];
-  //   if (duration != "All") {
-  //     tempRecipes = tempRecipes.filter((recipe) => {
-  //       if (job.type.toLowerCase() == filter.toLowerCase()) {
-  //         return true;
-  //       } else {
-  //         return false;
-  //       }
-  //     });
-  //   }
-  // }
+  function runSearch(recipeList, text) {
+    if (text != "") {
+      const inputText = text.toLowerCase();
+      const newRecipeList = recipeList.filter((recipe) => {
+        if (
+          recipe.course.toLowerCase().includes(inputText) ||
+          recipe.cuisine.toLowerCase().includes(inputText) ||
+          recipe.difficulty.toLowerCase().includes(inputText) ||
+          recipe.diet.toLowerCase().includes(inputText) ||
+          recipe.title.toLowerCase().includes(inputText)
+        ) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+      return newRecipeList;
+    } else if (text === "") {
+      return recipeList;
+    }
+  }
+
+  function whatToParse() {
+    let tempRecipes = [...recipesContext];
+    if (duration != "All") {
+      tempRecipes = tempRecipes.filter((recipe) => {
+        if (duration == "Less") {
+          if (parseInt(recipe.time) < 30) {
+            return true;
+          } else {
+            return false;
+          }
+        } else {
+          if (parseInt(recipe.time) >= 30) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      });
+    }
+    if (difficulty != "All") {
+      tempRecipes = tempRecipes.filter((recipe) => {
+        if (recipe.difficulty.toLowerCase() == difficulty.toLowerCase()) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+    }
+    if (cuisine != "All") {
+      tempRecipes = tempRecipes.filter((recipe) => {
+        if (recipe.cuisine.toLowerCase() == cuisine.toLowerCase()) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+    }
+    if (diet != "All") {
+      tempRecipes = tempRecipes.filter((recipe) => {
+        if (recipe.diet.toLowerCase() == diet.toLowerCase()) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+    }
+
+    tempRecipes = runSearch(tempRecipes, keyword);
+    return tempRecipes;
+  }
   console.log(recipesContext);
   return (
     <div className="grid-container">
-      {" "}
-      <RecipeCard />
-      <RecipeCard />
-      <RecipeCard />
-      <RecipeCard />
-      <RecipeCard />
-      <RecipeCard />
+      {recipesContext &&
+        whatToParse().map(
+          (recipe) =>
+            recipe.is_published == true && (
+              <RecipeCard
+                key={recipe.id}
+                image={recipe.image_url}
+                title={recipe.title}
+                id={recipe.id}
+              />
+            )
+        )}
     </div>
   );
 }
--- Client/src/Components/RecipeCard/RecipeCard.jsx
+++ Client/src/Components/RecipeCard/RecipeCard.jsx
@@ -6,21 +6,19 @@ import { FcLike } from "react-icons/fc";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
 
-export default function RecipeCard() {
+export default function RecipeCard({ image, title, id }) {
+  console.log(id);
   return (
     <Card
       style={{
         boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2",
       }}
     >
-      <Card.Img
-        variant="top"
-        src="https://media.istockphoto.com/id/833390070/photo/arabic-dishes-and-meze.jpg?s=612x612&w=0&k=20&c=MtWWS3THa19Mnb96iOGfpxOGwdmYNM-Xi0_zAEW9mTY="
-      />
+      <Card.Img variant="top" src={image} />
       <Card.Body>
         <Row>
           <Col xs={9}>
-            <Card.Title>Rice</Card.Title>
+            <Card.Title>{title}</Card.Title>
           </Col>
           <Col>
             <FcLike style={{ fontSize: "25px" }} />
